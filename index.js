2081f1c18a7b967e09ba05a9496019e097419a83
Sara Broad <sarabroad@Saras-MacBook-Pro-2.local>
2018-04-17 18:44:32 -0400

    first commit

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ var inquirer = require('inquirer');
 var Word = require("./word.js");
 var words = require("./words.js");
 
+// index.js: The file containing the logic for the course of the game, which depends on Word.js and:
 
-var newWord = new Word("dog");
-console.log(newWord.displayWord());
-newWord.guesser("s");
-console.log(newWord.displayWord());
-newWord.guesser("d");
-console.log(newWord.displayWord());
+
+// Randomly selects a word and uses the Word constructor to store it
+// Prompts the user for each guess and keeps track of the user's remaining guesses
+
+// var newWord = new Word(wordChoice)
+
+var guesses = 15;
+var wordChoices = ["Divided Sky", "Simple", "Squirming Coil", "The Lizards", "Tweezer", "Reba"];
+
+function guessLetters() {
+
+}
